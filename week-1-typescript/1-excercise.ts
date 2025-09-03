111e45cb63c0c68cd3dc7b3616bf09d1676e8f16
VictorKristiansson <Victor.Kristiansson@stud.sundsgarden.se>
2025-09-03 18:30:30 +0700

    initial commit
    initial commit

---
+++ week-1-typescript/1-excercise.ts
@@ -0,0 +1,16 @@
+// Excercise 1
+
+/*
+Create a type called IDType that can be either a number OR a string. 
+ Write a function showID that prints "Your ID is: ...". 
+ Call the function with both a number and a string. 
+ */ 
+
+ type IDType = string | number ;
+
+ const showID = (userId : IDType) =>  {
+  console.log(`Your ID is: ${userId}`);
+ }
+
+ showID("123");
+ showID(123);
